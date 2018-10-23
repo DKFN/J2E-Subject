5ed4c20fa81a9daadc4a48e139c400b3a25c2f46
vtetelin <valere.tetelin@epita.fr>
2018-10-23 12:20:36 +0200

    Initial commit legacy spring app

---
+++ legacy/monolithService/src/main/java/com/awesomecompany/megamonolith/MegamonolithApplication.java
@@ -0,0 +1,12 @@
+package com.awesomecompany.megamonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegamonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegamonolithApplication.class, args);
+	}
+}
